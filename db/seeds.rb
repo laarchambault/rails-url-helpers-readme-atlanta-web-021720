c5b4488441b9b39115958233da6570704a2f34fb
laarchambault <laarchambault@gmail.com>
2020-03-12 18:32:59 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create({title: "Always", description: "The things you can't remember"})
+Post.create({title: "Arelia", description: "The last fortune"})
